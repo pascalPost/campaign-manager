6993cfe2bff5168ad08e4cff78a074cfb9af1658
Pascal Post <pp-post@mailbox.org>
2024-02-06 12:48:18 +0100

    undo distDir setting

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   
     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
     // skipTrailingSlashRedirect: true,
-    distDir: "dist",
+    distDir: ".next",
     compress: false,
   };
   
