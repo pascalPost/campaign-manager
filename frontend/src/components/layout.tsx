e13c4467904644ddc8f6041417e1395b8a287dab
Pascal Post <pp-post@mailbox.org>
2024-04-06 10:43:07 +0200

    WIP: layout enhanced

---
+++ frontend/src/components/layout.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+import Header from "@/components/header";
+import { ScrollArea } from "@/components/ui/scroll-area.tsx";
+
+export const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <Header />
+      <div className="border-b">
+        <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+          <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 border-r border-border/40 md:sticky md:block">
+            <ScrollArea className="h-full py-6 pr-6 lg:py-8"></ScrollArea>
+          </aside>
+          <main className="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
+            {children}
+          </main>
+        </div>
+      </div>
+    </>
+  );
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,27 +6,12 @@ import DashboardPage from "./dashboard/page";
 import CreateProjectPage from "@/project/create/page";
 import ProjectPage from "@/project/[projectId]/page.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu.tsx";
-import { ModeToggle } from "@/components/mode-toggle.tsx";
+import { Layout } from "@/components/layout";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="m-4 flex-col">
-        <div className="pb-2 pt-2">
-          <NavigationMenu className="justify-end">
-            <NavigationMenuList className="justify-end">
-              <NavigationMenuItem>
-                <ModeToggle />
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
-
+      <Layout>
         <Switch>
           <Route path="/" component={DashboardPage} />
           <Route path="/project/create" component={CreateProjectPage} />
@@ -35,7 +20,7 @@ createRoot(document.getElementById("root")!).render(
           {/*/!* Default route in a switch *!/*/}
           <Route>404: No such page!</Route>
         </Switch>
-      </div>
+      </Layout>
     </ThemeProvider>
   </StrictMode>,
 );
